421cb985679a9ad2fabe63d040f38c1cec3a2574
Bo <zannb@outlook.com>
2017-03-25 22:41:01 +0800

    改成utf8编码，keil真的服。。

--- main.c
+++ main.c
@@ -13,31 +13,31 @@
 	u8 len;	
 	u16 times=0; 
 	char h[5]={0xaa,0xaa,0xaa,0xaa,0xaa};
-	delay_init();	    	 //延时函数初始化	
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);// 设置中断优先级分组2
-	uart1_init(9600);	 //串口初始化为9600
+	delay_init();	    	 //
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//
+	uart1_init(9600);	 //
 	//uart2_init(9600);
-	LED_Init();		  	 //初始化与LED连接的硬件接口
+	LED_Init();		  	 //
 	delay_ms(100);
 	USART1_send_buf("uart1 hahah\r\n");
 	//USART1_send_buf(h);
 	//USART2_send_buf("uart2 hahah\r\n");
 	//USART2_send_buf(h);
-	USART1_send_buf("\r\n您发送的消息为:\r\n");
+	USART1_send_buf("\r\n鍚冧釜澶ч钑塡r\n");
 	while(1)
 	{
 		if(USART1_RX_STA&0x8000)
 		{					   
-			len=USART1_RX_STA&0x3fff;//得到此次接收到的数据长度
-			USART1_send_buf("\r\n您发送的消息为:\r\n");
+			len=USART1_RX_STA&0x3fff;//浠�涔堟儏鍐靛晩
+			USART1_send_buf("\r\n鐪熺殑鏈嶄簡\r\n");
 			USART1_send_buf((char *)USART1_RX_BUF);
 			//USART2_send_buf((char *)USART1_RX_BUF);
 			strcpy(USART1_RX_BUF,"\0");
 			USART1_RX_STA=0;
 		}
 		else if(USART2_RX_STA&0x8000){
-			len=USART2_RX_STA&0x3fff;//得到此次接收到的数据长度
-			USART1_send_buf("\r\n您接接收的消息为:\r\n");
+			len=USART2_RX_STA&0x3fff;//
+			USART1_send_buf("\r\n浠�涔堢嫍灞佺紪鐮乗r\n");
 			USART1_send_buf((char *)USART2_RX_BUF);
 			strcpy(USART2_RX_BUF,"\0");
 			USART2_RX_STA=0;
